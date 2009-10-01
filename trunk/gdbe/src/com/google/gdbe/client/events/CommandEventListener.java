51adcfc9aeafd6a35e98fe471da5a62951eb10b5
bobbysoares <bobbysoares@a99bb848-095d-11de-98bb-07cc9828235c>
2009-10-01 06:25:15 +0000

    Initial version of GDBE.

---
+++ trunk/gdbe/src/com/google/gdbe/client/events/CommandEventListener.java
@@ -0,0 +1,15 @@
+package com.google.gdbe.client.events;
+
+import java.util.EventListener;
+
+/**
+ * Defines the command event listener interface.
+ */
+public interface CommandEventListener extends EventListener {
+  
+  /**
+   * Fired whenever a command event is received.
+   * @param e
+   */
+  void onCommand(CommandEvent e);
+}
